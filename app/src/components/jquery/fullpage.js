9b96ee2919271de01dd4299008a655e0dcc372f8
Marin Petkov <mpetkov87@gmail.com>
2015-10-24 13:17:15 -0500

    Created slideshow and styling for the splash page

--- app/src/components/jquery/fullpage.js
+++ app/src/components/jquery/fullpage.js
@@ -2,7 +2,7 @@
 {
 	'use strict';
 
-	var fullpage = function ($timeout, $interval, configConstant)
+	var fullpage = function ($rootScope, $timeout, $interval, configConstant)
 	{
 		var interval;
 
@@ -15,42 +15,45 @@
 					$timeout(function ()
 					{
 						$(element).fullpage
-								({
-									navigation: true,
-									css3: false,
-									responsiveHeight: configConstant.setup.minHeight,
-									controlArrows: false,
-									afterRender: function () {
-										//on page load, start the slideshow
-										interval = $interval(function () {
-											$.fn.fullpage.moveSlideRight();
-										}, 5000);
-									},
-									onLeave: function (index, nextIndex, direction)
-									{
-										if (index == '1') 
-										{
-											$interval.cancel(interval);
-										}
-		
-		
-										if (configConstant.content[nextIndex - 1])
-										{
-											$('#fp-nav').find('span').css('background-color', '#' + configConstant.content[nextIndex - 1].color);
-											$('.navigation .bar').css('background-color', '#' + configConstant.content[nextIndex - 1].color);
-											$('.navigation path').css('fill', '#' + configConstant.content[nextIndex - 1].color);
-										}
-									},
-									onSlideLeave: function (anchorLink, index, slideIndex, direction) {
-										$.fn.fullpage.setScrollingSpeed(0);
-										$('.fp-section').find('.fp-slidesContainer').hide();
-									},
-									// Display the slides container by fading it in after the next slide has been loaded.
-									afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-										$('.fp-section').find('.fp-slidesContainer').fadeIn(700);
-										$.fn.fullpage.setScrollingSpeed(700);
-									}
-								});
+						({
+							navigation: true,
+							css3: false,
+							responsiveHeight: configConstant.setup.minHeight,
+							controlArrows: false,
+							afterRender: function () 
+							{
+								$rootScope.$broadcast('fullpage::ready');
+								console.log('1111');
+								//on page load, start the slideshow
+								/*interval = $interval(function () {
+									$.fn.fullpage.moveSlideRight();
+								},8000);*/
+							},
+							onLeave: function (index, nextIndex, direction)
+							{
+								if (index == '1') 
+								{
+									$interval.cancel(interval);
+								}
+
+
+								if (configConstant.content[nextIndex - 1])
+								{
+									$('#fp-nav').find('span').css('background-color', '#' + configConstant.content[nextIndex - 1].color);
+									$('.navigation .bar').css('background-color', '#' + configConstant.content[nextIndex - 1].color);
+									$('.navigation path').css('fill', '#' + configConstant.content[nextIndex - 1].color);
+								}
+							},
+							onSlideLeave: function (anchorLink, index, slideIndex, direction) {
+								//$.fn.fullpage.setScrollingSpeed(0);
+								//$('.fp-section').find('.fp-slidesContainer').hide();
+							},
+							// Display the slides container by fading it in after the next slide has been loaded.
+							afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+								//$('.fp-section').find('.fp-slidesContainer').fadeIn(700);
+								//$.fn.fullpage.setScrollingSpeed(700);
+							}
+						});
 					}, 400);
 
 				});
@@ -58,5 +61,5 @@
 		};
 	};
 
-	angular.module('portfolio').directive('fullpage', ['$timeout', '$interval', 'configConstant', fullpage]);
+	angular.module('portfolio').directive('fullpage', ['$rootScope', '$timeout', '$interval', 'configConstant', fullpage]);
 })();
