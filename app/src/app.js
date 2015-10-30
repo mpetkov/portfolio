8714b145382f7b304d83f6556b943a7569f86db7
Marin Petkov <mpetkov87@gmail.com>
2015-10-30 15:22:14 -0500

    Finished testing all sizes using google chrome

--- app/src/app.js
+++ app/src/app.js
@@ -15,7 +15,34 @@
 		$translateProvider.useSanitizeValueStrategy('sanitize');
 		$translateProvider.use(configConstant.setup.language);
 	};
+	
+	var run = function (deviceDetector)
+	{
+		console.log(deviceDetector);
+		console.log(deviceDetector.isDesktop());
+	};
+	
+	var init = function (deviceDetector)
+	{
+		return function (scope, element, attrs)
+		{
+			var useragent = deviceDetector.os + ' ' + deviceDetector.browser + ' ' + deviceDetector.browser_version + ' ';
+			
+			if(deviceDetector.isDesktop())
+			{
+				useragent += 'desktop';
+			}
+			else
+			{
+				useragent += 'mobile';
+			}
+			
+			
+			scope.useragent = useragent;
+		};
+	};
 
-	angular.module('portfolio', ['pascalprecht.translate', 'ngSanitize'])
-			.config(['$logProvider', '$translateProvider', 'configConstant', config]);
+	angular.module('portfolio', ['pascalprecht.translate', 'ngSanitize', 'ng.deviceDetector'])
+			.config(['$logProvider', '$translateProvider', 'configConstant', config])
+			.directive('init', ['deviceDetector', init]);
 })();
